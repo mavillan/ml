2ff2b028c478d354767d28ae94ed8ef07dd9aa86
Your Name <you@example.com>
2015-11-16 02:47:24 -0300

    generated train and test dataset for faces94-96

--- project/scripts/generating_datasets.py
+++ project/scripts/generating_datasets.py
@@ -6,55 +6,59 @@ import shutil
 """
 Script Usage
 
-> execute it in ml/project/ directory as: python2 scripts/generating_datasets.py n_train
-> n_train is the number for samples per class in training set
-> The remaining samples will be copied in testing set
+> execute it in ml/project/ directory as: 
+  python2 scripts/generating_datasets.py <inp_db>  <n_train>
+> <inp_db> input database: faces94, faces95 or faces96 as string
+> <n_train> is the number for samples per class in training set
+> The remaining samples will be copied in testing :set
 """
 
 
-#path were original files are
-path = './faces/'
-
-#verify parameters
-#n_train is the number of training samples per class
-if len(sys.argv)>2:
+#verify parameters are valid
+if len(sys.argv)>3:
 	sys.exit('too much parameters!')
-elif len(sys.argv)==2:
-	n_train = int(sys.argv[1])
+elif len(sys.argv)<3:
+	sys.exit('missing parameters!')
 else:
-	n_train = 5
+	inp_db = sys.argv[1]
+	if inp_db not in ['faces94','faces95','faces96']:
+		sys.exit('database not valid!')
+	n_train = int(sys.argv[2])
+	if n_train<1 or n_train>5:
+		sys.exit('wrong number of samples per class!')
+	
+#set some variables according to input parameters
+path = './db/'+inp_db+'/'                                    #source path
+tr_path = './db/train'+inp_db[-2:]+'/'+str(n_train)+'pc/'    #training path
+ts_path = './db/test'+inp_db[-2:]+'/'+str(20-n_train)+'pc/'  #testing path
+N = len(os.listdir(path))                                    #total number classes (people)
 
-#create target directories
-if os.path.exists('./training_set/'):
-	sys.exit('training_set directory already exists!')
-else:
-	os.makedirs('./training_set/')
 
-if os.path.exists('./testing_set/'):
-	sys.exit('training_set directory already exists!')
+if os.path.exists(tr_path):
+	sys.exit(tr_path+' directory already exists!')
 else:
-	os.makedirs('./testing_set/')
+	os.makedirs(tr_path)
+
+if os.path.exists(ts_path):
+	sys.exit(ts_path+' directory already exists!')
+else:
+	os.makedirs(ts_path)
 
 #iterate through all files in ./faces directory
-for i in range(1,153):
+for i in range(1,N):
 	tgt = path+str(i)+'/'
 	files = os.listdir(tgt)
-	#shuffle the files to make it fair
+	#shuffle the filenames to make it fair
 	random.shuffle(files)
 
 	#create target directories
-	os.makedirs('./training_set/'+str(i))
-	os.makedirs('./testing_set/'+str(i))
+	os.makedirs(tr_path+str(i))
+	os.makedirs(ts_path+str(i))
 
-	#copy the first 5 photos in training and the remaining
-	#15 in testing directories
+	#copy the first n_train photos in train and the remaining
+	#20-n_train in test directories
 	for j in range(n_train):
-		shutil.copy(tgt+files[j], './training_set/'+str(i))
+		shutil.copy(tgt+files[j], tr_path+str(i))
 	for j in range(n_train,20):
-		shutil.copy(tgt+files[j], './testing_set/'+str(i))
-
-
-	
-		
-
-
+		shutil.copy(tgt+files[j], ts_path+str(i))
+print "Done!"
